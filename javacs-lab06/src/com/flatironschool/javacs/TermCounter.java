1d1b8e14a4e631fe2cec3953919437100188f2b6
CeciliaLiu <ziyue.liu7@gmail.com>
2016-06-04 18:32:30 -0500

    Done.

--- javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
+++ javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
@@ -37,7 +37,11 @@ public class TermCounter {
 	 */
 	public int size() {
         // TODO: fill this in.
-		return -1;
+        int size = 0;
+        for (String key: keySet()) {
+        	size += get(key);
+        }
+		return size;
 	}
 
 	/**
